4cc41c6f50fa47b9840ed46f2793e1b59648a9c0
DeStiny555555555555 <nik.okhrimenko.2000@mail.ru>
2024-10-28 15:33:19 +0300

    Complete Homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Ф.И.О. сотрудника - " + fullName + ".");
+
+        System.out.println("Задание 2");
+
+        String fullName1 = "ivanov ivan ivanovich";
+        System.out.println(fullName1.toUpperCase());
+
+        System.out.println("Задание 3");
+        String fullName2 = "Иванов Сёмен Семёнович";
+        fullName2 = fullName2.replace("ё", "е");
+        System.out.println("Данные Ф.И.О. сотрудника - " + fullName2);
     }
 }
